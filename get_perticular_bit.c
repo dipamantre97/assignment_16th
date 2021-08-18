5463a35711ba67a6ab696fe051b53e719a3c7c9e
dipamantre97 <87062425+dipamantre97@users.noreply.github.com>
2021-08-18 10:17:19 +0530

    Add files via upload

---
+++ get_perticular_bit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int data,bit;
+	printf("enter data\n");
+	scanf("%d",&data);
+	printf("enter bit\n");
+	scanf("%d",&bit);
+	printf("to get perticular bit=%d",(data>>bit)&1);
+	
+}
+/*
+enter data
+5  00000101
+enter bit
+0
+to get perticular bit=1
+*/
